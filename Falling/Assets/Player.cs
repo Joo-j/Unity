b8627bc7d478d649410fa5130989ddf4586a3587
Joo <auddus7975@naver.com>
2020-07-27 19:11:35 +0900

    check7

---
+++ Falling/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Time.timeScale += Time.fixedDeltaTime * 0.01f;
+        transform.rotation *= Quaternion.Euler(0, 0, Time.deltaTime * 7f);
+        rb.velocity += transform.rotation * (Input.GetAxisRaw("Horizontal") * Vector3.right) * 10f * Time.deltaTime;
+        rb.velocity += transform.rotation * (Input.GetAxisRaw("Vertical") * Vector3.up) * 10f * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Falling/Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject goSpawn;
+    public float fDifficulty = 10f;
+
+    float fSpawn = 0;
+
+	
+	// Update is called once per frame
+	void Update () {
+        fSpawn += fDifficulty * Time.deltaTime;
+        fDifficulty += Time.deltaTime * 4f;
+
+        while (fSpawn > 1)
+        {
+            fSpawn -= 1;
+            Vector3 v3Pos = new Vector3(Random.value * 40f - 20f, 0, Random.value * 40f - 20f) + transform.position;
+            GameObject goCreate = Instantiate(goSpawn, v3Pos, Quaternion.Euler(0, Random.value*360f, Random.value*30f));
+        }
+	}
+}
