e7050b01058c15ef4e8e7c26c5ebe807490980de
Joo <auddus7975@naver.com>
2020-07-27 19:27:36 +0900

    10

---
+++ ColorCircle/Assets/ColorSeter.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColorSeter : MonoBehaviour {
+
+    public string currentColor;
+    public float time = 4f;
+    public float timeSpeed = 1.0f;
+    string textColor;
+
+    void Start()
+    {
+        SetRandomColor();        
+    }
+
+    void Update()
+    {
+        Text uiText = GetComponent<Text>();
+
+        time -= Time.deltaTime * timeSpeed;
+        int t = Mathf.FloorToInt(time);
+        uiText.text = textColor + currentColor + "(" + t.ToString() + ")" + "</color>";
+     
+            timeCounter();
+    }
+
+    void SetRandomColor()
+    {
+        int index = Random.Range(0, 3);
+
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                textColor = "<color=#000fff>";
+                break;
+            case 1:
+                currentColor = "Yellow";
+                textColor = "<color=#00ff00>";
+                break;
+            case 2:
+                currentColor = "Pink";
+                textColor = "<color=#f00000>";
+                break;
+            case 3:
+                currentColor = "Magenta";
+                textColor = "<color=#0000ff>";
+                break;
+        }
+    }
+
+        public void timeCounter()
+        {
+            if (time <= 0)
+            {
+                time = 4;
+                timeSpeed += 0.1f;
+                SetRandomColor();
+            }
+        }
+    }
---
+++ ColorCircle/Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float moveForce = 100f;
+    float movement = 0f;
+    ColorSeter cs;
+
+    void Start()
+    {
+        cs = GetComponent<ColorSeter>();
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * moveForce);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {       
+        if (cs.time <= 0.01)
+        {
+            if (col.tag != cs.currentColor)
+            {
+                Debug.Log("GAME OVER!");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+}
