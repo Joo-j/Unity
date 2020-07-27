a39561e5978efc16f23f148c1a68d2e5ca132364
Joo <auddus7975@naver.com>
2020-07-27 19:53:44 +0900

    123

---
+++ PunchAwayAHead/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public Animator animator;
+    //public Animation[] attack; 
+
+    void Start()
+    {
+        animator.Play("Run");
+
+    }
+
+    void Update()
+    {
+        //transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // int rand = Random.Range(0, attack.Length);
+       // string a = attack[rand].ToString();
+        if (other.tag != gameObject.tag)
+        {
+               animator.Play("Jab");
+        }
+    }
+
+
+}
