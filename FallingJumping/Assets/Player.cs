ba6a8829e216cdb069a39eba9ffc21bc7fd4678d
Joo <auddus7975@naver.com>
2020-07-27 19:08:11 +0900

    check6

---
+++ FallingJumping/Assets/Player.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody rb;
+    Animator am;
+    private float h;
+    private float v;
+    private float moveX;
+    private float moveZ;
+    public float speedH = 50f;
+    public float speedZ = 80f;
+    public float speedJ = 10f;
+    public float speedT = 10f;
+
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        am = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Time.timeScale += Time.fixedDeltaTime * 0.01f;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            am.Play("JUMP00", -1, 0);
+            rb.velocity = new Vector3(0, speedJ, 0);
+        }
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        am.SetFloat("h", h);
+        am.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+        if (moveZ <= 0)
+        {
+            moveX = 0;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            if (v >= 0)
+            {
+                this.transform.Rotate(0, -speedT * 10 * Time.deltaTime, 0);
+            }
+            else
+            {
+                this.transform.Rotate(0, speedT * 10 * Time.deltaTime, 0);
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.R))
+        {
+            if (v >= 0)
+            {
+                this.transform.Rotate(0, speedT * 10 * Time.deltaTime, 0);
+            }
+            else
+            {
+                this.transform.Rotate(0, -speedT * 10 * Time.deltaTime, 0);
+            }
+        }
+        rb.velocity = new Vector3(moveX, 0, 0); 
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Floor")
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+       }
+    }
+
+
---
+++ FallingJumping/Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject goSpawn;
+    public float fDifficulty;
+
+    float fSpawn = 0;
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        fSpawn += fDifficulty * Time.deltaTime;
+        fDifficulty += Time.deltaTime * 1f;
+
+        while (fSpawn > 2)
+        {
+            fSpawn -= 2;
+            Vector3 v3Pos = new Vector3(Random.value * 40f - 20f, 0, Random.value * 40f - 20f) + transform.position;
+            GameObject goCreate = Instantiate(goSpawn, v3Pos, Quaternion.Euler(0, Random.value*360f, Random.value*30f));
+        }
+	}
+}
